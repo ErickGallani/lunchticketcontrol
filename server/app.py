34ef03a78489ba39ecbf95d0321ca1dde6a79202
Erick Gallani <erick.gallani.1@gmail.com>
2017-12-22 00:54:03 +0000

    JWT implemented

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask_restful_swagger_2 import Resource, swagger
 from setup import api, app
+from flask_jwt import jwt_required
 
 class HelloWorld(Resource):
     @swagger.doc({
@@ -17,10 +18,11 @@ class HelloWorld(Resource):
             }
         }
     })
+    @jwt_required()
     def get(self):
         return {'hello': 'world'}, 200
 
 api.add_resource(HelloWorld, '/api')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- server/setup.py
+++ server/setup.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_restful_swagger_2 import Resource, Api
 from flask_swagger_ui import get_swaggerui_blueprint
+from flask_jwt import JWT
+from auth.security import authenticate, identity
 
 SWAGGER_URL = '/api/docs'
-API_URL = 'http://127.0.0.1:5000/api/swagger.json'
+API_URL = 'http://127.0.0.1:5050/api/swagger.json'
 
 app = Flask(__name__)
+app.config.from_pyfile('settings.cfg', silent=True)
 api = Api(app, api_version='1.0', api_spec_url='/api/swagger')
+
+jwt = JWT(app, authenticate, identity)
 
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
