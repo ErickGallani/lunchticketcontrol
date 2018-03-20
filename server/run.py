b3144da2526b919a9b27316256bd5b10d002526f
Erick Gallani <erick.gallani.1@gmail.com>
2018-03-20 01:51:01 +0000

    Refactor run.py to re-use the app config object instance

--- server/run.py
+++ server/run.py
@@ -1,16 +1,13 @@
 """ Runs the lunchticket webapi """
 import os
 from app import get_app, create_app
-from app.config import get_app_config
 
 CONFIG_TYPE = os.getenv('APP_SETTINGS') or 'development'
 
 APP = get_app(create_app)(CONFIG_TYPE)
 
-CONFIG = get_app_config(CONFIG_TYPE)
-
 if __name__ == '__main__':
-    APP.run(host=CONFIG.HOST,
-            port=CONFIG.PORT,
+    APP.run(host=APP.config.get('HOST'),
+            port=APP.config.get('PORT'),
             ssl_context=('cert/localhost.crt',
                          'cert/localhost.key'))
